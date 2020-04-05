04f494b9c2cc493c01ffa38c9596d37ee548e7d3
Kyzyl Monteiro <kyzylmonteiro@gmail.com>
2020-04-05 13:45:36 +0530

    Updates

---
+++ precache-manifest.468a7a79ad29d99abb5a53acaa36eac0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af1739fd59a9b803ebb625ced2bde4b",
+    "url": "/reviewpedia/index.html"
+  },
+  {
+    "revision": "9a4b9a6f59142bbfe040",
+    "url": "/reviewpedia/static/css/main.91375b30.chunk.css"
+  },
+  {
+    "revision": "5b3e5731f11246a7460a",
+    "url": "/reviewpedia/static/js/2.418a789b.chunk.js"
+  },
+  {
+    "revision": "9a4b9a6f59142bbfe040",
+    "url": "/reviewpedia/static/js/main.9d706b19.chunk.js"
+  },
+  {
+    "revision": "be768306b987a3287968",
+    "url": "/reviewpedia/static/js/runtime~main.7ad722fc.js"
+  },
+  {
+    "revision": "ebadb179def681b41006bc161724c089",
+    "url": "/reviewpedia/static/media/bg.ebadb179.png"
+  },
+  {
+    "revision": "fd5f6b5ac3f588e226adf6d22f4307cd",
+    "url": "/reviewpedia/static/media/img1.fd5f6b5a.png"
+  },
+  {
+    "revision": "c4f847b47ed8dc6b414046571e74add5",
+    "url": "/reviewpedia/static/media/img2.c4f847b4.png"
+  },
+  {
+    "revision": "5a9dfa590d60306ec604c69cb93470d2",
+    "url": "/reviewpedia/static/media/img3.5a9dfa59.png"
+  },
+  {
+    "revision": "7e4a3c70807f8ea47de2f81914a05411",
+    "url": "/reviewpedia/static/media/img4.7e4a3c70.gif"
+  },
+  {
+    "revision": "08743c5a6c56bb83549d1b391ed4a750",
+    "url": "/reviewpedia/static/media/img5.08743c5a.png"
+  },
+  {
+    "revision": "7e5741e9da9bf53df1e7db04f85b2005",
+    "url": "/reviewpedia/static/media/img6.7e5741e9.png"
+  },
+  {
+    "revision": "e346456a626ad1ce21f0f1ccb80bb2a1",
+    "url": "/reviewpedia/static/media/img7.e346456a.png"
+  }
+]);

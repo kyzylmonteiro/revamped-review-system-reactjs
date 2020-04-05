4fd1d8e3e6db90545425249dec4e63083238037c
Kyzyl Monteiro <kyzylmonteiro@gmail.com>
2020-04-05 13:48:26 +0530

    Updates

---
+++ precache-manifest.7b5bde4ea6ecf41d3e543b93961dfb7f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86535c7b2d47557dee72f34203e53d94",
+    "url": "/revamped-review-system-reactjs/index.html"
+  },
+  {
+    "revision": "2cbe54271256cf75e0f2",
+    "url": "/revamped-review-system-reactjs/static/css/main.3e9d506b.chunk.css"
+  },
+  {
+    "revision": "5b3e5731f11246a7460a",
+    "url": "/revamped-review-system-reactjs/static/js/2.418a789b.chunk.js"
+  },
+  {
+    "revision": "2cbe54271256cf75e0f2",
+    "url": "/revamped-review-system-reactjs/static/js/main.9d706b19.chunk.js"
+  },
+  {
+    "revision": "d87ff2d0fee74d08ea85",
+    "url": "/revamped-review-system-reactjs/static/js/runtime~main.c1487d80.js"
+  },
+  {
+    "revision": "ebadb179def681b41006bc161724c089",
+    "url": "/revamped-review-system-reactjs/static/media/bg.ebadb179.png"
+  },
+  {
+    "revision": "fd5f6b5ac3f588e226adf6d22f4307cd",
+    "url": "/revamped-review-system-reactjs/static/media/img1.fd5f6b5a.png"
+  },
+  {
+    "revision": "c4f847b47ed8dc6b414046571e74add5",
+    "url": "/revamped-review-system-reactjs/static/media/img2.c4f847b4.png"
+  },
+  {
+    "revision": "5a9dfa590d60306ec604c69cb93470d2",
+    "url": "/revamped-review-system-reactjs/static/media/img3.5a9dfa59.png"
+  },
+  {
+    "revision": "7e4a3c70807f8ea47de2f81914a05411",
+    "url": "/revamped-review-system-reactjs/static/media/img4.7e4a3c70.gif"
+  },
+  {
+    "revision": "08743c5a6c56bb83549d1b391ed4a750",
+    "url": "/revamped-review-system-reactjs/static/media/img5.08743c5a.png"
+  },
+  {
+    "revision": "7e5741e9da9bf53df1e7db04f85b2005",
+    "url": "/revamped-review-system-reactjs/static/media/img6.7e5741e9.png"
+  },
+  {
+    "revision": "e346456a626ad1ce21f0f1ccb80bb2a1",
+    "url": "/revamped-review-system-reactjs/static/media/img7.e346456a.png"
+  }
+]);
